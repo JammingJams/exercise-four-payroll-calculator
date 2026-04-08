d702614b617ab65ef4d49f849bc5715210213e78
JammingJams <noahlogan20o00@gmail.com>
2026-04-08 15:27:17 -0400

    Forgot to add these...

--- src/main/java/com/pluralsight/PayRollCalculatorApp.java
+++ src/main/java/com/pluralsight/PayRollCalculatorApp.java
@@ -1,4 +1,23 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayRollCalculatorApp {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String firstName = keyboard.nextLine();
+
+        System.out.print("Enter amount of hours worked: ");
+        double hoursWorked = keyboard.nextDouble();
+
+        System.out.print("Enter pay rate: ");
+        double payRate = keyboard.nextDouble();
+
+        System.out.printf("Your name is %s and your gross income is $%.2f", firstName, hoursWorked * payRate);
+
+
+
+    }
 }
